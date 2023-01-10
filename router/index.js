6259b7e5bf997dbd204d7b214a22777175b247dc
NiBog <nokaut05@gmail.com>
2023-01-10 11:01:57 +0300

    add lite server

--- router/index.js
+++ router/index.js
@@ -1,9 +1,16 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import Home from '../views/home.vue'
 
 Vue.use(Router);
 
-const ROUTES = []
+const ROUTES = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    },
+]
 
 const router = new Router({
     routes: ROUTES,
---
+++ views/home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    HEllo World
+  </div>
+</template>
