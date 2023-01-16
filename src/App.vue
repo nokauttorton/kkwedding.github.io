6c94fa33e8308f2ca9200df9e2e88dbbd5ee27e3
NiBog <nokaut05@gmail.com>
2023-01-16 23:20:57 +0300

    version 1.0.1

--- src/App.vue
+++ src/App.vue
@@ -8,51 +8,14 @@
 
     export default {
         name: 'App',
-        // data() {
-        //     return {
-        //         timeout: false,
-        //         delta: 200,
-        //         rtime: 0
-        //     }
-        // },
         methods: {
-            // onResize() {
-            //     this.rtime = new Date();
-            //     if (!this.timeout) {
-            //         this.$set(this, 'timeout', true);
-            //         setTimeout(this.resizeEnd, 200);
-            //     }
-            // },
-            // resizeEnd() {
-            //     if (new Date() - this.rtime < this.delta) {
-            //         setTimeout(this.resizeEnd, this.delta);
-            //     } else {
-            //         this.$set(this, 'timeout', false);
-            //         this.updateDeviceType();
-            //         console.log("updateDeviceType", this.isTablet);
-            //     }
-            // }
+            
         },
         mounted() {
-            // if (document && document.body.style.zoom !== 0.9) {
-            // TODO: не пользуем такой скейл пока не определимся с необходимой шириной. В классе app-container есть адаптивность
-            //     document.body.style.zoom = 0.9
-            // }
-            // window.addEventListener('resize',  this.getResize);
-            // if (this.$store.getters.isRouteOnCreate) {
-            //     this.$nextTick(() => {
-            //         let data = this.$store.getters.getRouteOnCreate;
-            //         this[data.method](data.name, data.options);
-            //         this.$store.dispatch('clearRoute');
-            //     });
-            // }
-            // this.updateDeviceType();
-            // this.$nextTick(() => {
-            //     window.addEventListener('resize', this.onResize)
-            // })
+            
         },
         beforeDestroy() {
-            // window.removeEventListener('resize', this.onResize);
+            
         }
     }
 </script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,22 @@
 <template>
-  <div>
-    Hello World
+  <div class="x">
+    {{ xxx }}
   </div>
 </template>
 
 <script>
 export default {
   name: 'home',
+  computed: {
+    xxx() {
+      return window.innerWidth
+    }
+  },
   mounted() {
     console.log('MOUNT HOME')
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 </style>
